9d3d7f3776b7150f62af1fd39859685aa0585611
chennaijia <naijia030@gmail.com>
2024-10-29 17:27:25 +0800

    feat: personal page create

---
+++ pages/user/[id].vue
@@ -0,0 +1,17 @@
+<template>
+ 
+              <div class="flex flex-col gap-4">
+                <p class="text-4xl text-gray-600 font-semibold">陳乃嘉</p>
+                <p class="text-xl text-gray-600 font-semibold">@{{ id }}</p>
+              </div>
+
+  </template>
+  
+  
+    
+  <script setup>
+    const route = useRoute()
+    const { id } = route.params
+  </script>
+  
+  
