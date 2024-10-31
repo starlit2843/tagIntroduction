424bc5528296a417a9e73b2f007a6b0e5d2085d4
chennaijia <naijia030@gmail.com>
2024-10-31 15:36:13 +0800

    feat: add the changes when step changes

---
+++ components/tag_introduction/bottom_bar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="flex justify-center items-center">
+      <div class="flex gap-5">
+        <!-- 第一頁 -->
+        <div
+          v-if="activeIndex == 1"
+          class="specialcta-btn shadow-special-cta-02 rounded-30 w-24 py-1"
+        ></div>
+        <div
+          v-else
+          class="relative w-24 py-1 flex cursor-default rounded-full shadow-numo-light-inset"
+        ></div>
+  
+        <!-- 第二頁 -->
+        <div
+          v-if="activeIndex == 2"
+          class="specialcta-btn shadow-special-cta-02 rounded-30 w-24 py-1"
+        ></div>
+        <div
+          v-else
+          class="relative w-24 py-1 flex cursor-default rounded-full shadow-numo-light-inset"
+        ></div>
+  
+        <!-- 第三頁 -->
+        <div
+          v-if="activeIndex == 3"
+          class="specialcta-btn shadow-special-cta-02 rounded-30 w-24 py-1"
+        ></div>
+        <div
+          v-else
+          class="relative w-24 py-1 flex cursor-default rounded-full shadow-numo-light-inset"
+        ></div>
+  
+        <!-- 第四頁 -->
+        <div
+          v-if="activeIndex == 4"
+          class="specialcta-btn shadow-special-cta-02 rounded-30 w-24 py-1"
+        ></div>
+        <div
+          v-else
+          class="relative w-24 py-1 flex cursor-default rounded-full shadow-numo-light-inset"
+        ></div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  // 接收 activeIndex 作為 prop 來控制按鈕位置
+  const props = defineProps({
+    activeIndex: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  });
+  </script>
+  
+  <style scoped>
+  /* 如有需要可以在這裡加上樣式 */
+  </style>
+  
--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,28 +1,66 @@
 <template>
   <div class="flex justify-around py-4 max-w-screen-lg mx-auto">
-    <div class="flex flex-col gap-4 bg-gray-100">
+    <div class="flex flex-col gap-8 bg-gray-100">
       <div class="flex justify-between items-center grid-cols-3">
-        <p></p>
+        <span class="last_step text-2xl font-semibold cursor-pointer" @click="lastStep" v-if="currentStep>1">
+          上一步
+        </span>
+        <span class="last_step text-2xl font-semibold cursor-pointer" @click="lastStep" v-else>
+
+        </span>
         <p class="text-2xl text-gray-300 font-semibold text-center">
           限時連結功能指引
         </p>
-        <span class="skip_tutorial text-2xl text-green-300 font-semibold cursor-pointer" @click="skipTutorial">
+        <span class="skip_tutorial text-2xl font-semibold cursor-pointer" @click="skipTutorial">
           跳過教學
         </span>
       </div>
       <iframe src="https://opennccu.com/u/Naijia" width="500" height="500" scrolling="yes" align="center"></iframe>
-      <img src="~/assets/images/tutorial_1.png" width="500" alt="進度條圖示"/>
+      <BottomBar :activeIndex="currentStep" />
+      <div class="flex justify-center" v-if="currentStep<4">
+        <button class="specialcta-btn shadow-special-cta-02 place-self-center rounded-10 px-6 py-3" @click="nextStep">
+          <span class="text-[2vw] text-white min-[300px]:text-xl">下一步</span>
+        </button>
+      </div><div class="flex justify-center" v-if="currentStep==4">
+        <button class="specialcta-btn shadow-special-cta-02 place-self-center rounded-10 px-6 py-3" @click="nextStep">
+          <span class="text-[2vw] text-white min-[300px]:text-xl">馬上去設置</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+  import '~/assets/css/tailwind base.css';
+  import { ref } from 'vue';
+  import { useRoute } from 'vue-router';
+  import BottomBar from '~/components/tag_introduction/bottom_bar.vue';
 
-const route = useRoute();
-const { id } = route.params;
+  const route = useRoute();
+  const { id } = route.params;
 
-function skipTutorial() {
-  alert("跳過教學");
-}
+  const currentStep = ref(1);
+  const maxSteps = 4;
+
+  function skipTutorial() {
+    alert("跳過教學");
+  }
+
+  function nextStep() {
+    if (currentStep.value < maxSteps) {
+      currentStep.value += 1;
+    }
+  }
+
+  function lastStep() {
+    if (currentStep.value > 1) {
+      currentStep.value -= 1;
+    }
+  }
 </script>
+
+<style lang="css">
+  .skip_tutorial, .last_step{
+    color:darkseagreen;
+  }
+</style>
